9248b077b20c8c44cba1b1a87aac117e139c8d9a
Mahbub Bello <mahbubbello@gmail.com>
2021-12-03 01:06:08 +0100

    added a book route

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const bookRouter = express.Router()
 
-app.use(express.json())
+app.use(express.json());
 
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-    console.log(`Server running on https//localhost:${port}`)
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+bookRouter.route('/books')
+.get((req, res) => {
+  const response = { Hello: 'This is my API'}
+
+  res.json(response)
 })
+app.use('/api', bookRouter)
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
